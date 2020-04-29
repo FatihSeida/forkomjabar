7711a89b85c7f54a4cf407c8dc876e321de2d1ee
FatihSeida <fatihseidaaaaa@gmail.com>
2020-04-29 09:20:41 +0700

    Revisi

---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,60 @@
+<footer class="footer-area" style="margin-top: 50px;">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-sm-6 col-md-4 col-xl-3">
+                <div class="single-footer-widget footer_1">
+                    <h4>Tentang</h4>
+                    <a href="#"> <!-- <img src="img/logo.png" alt=""> --> </a>
+                    <p>Visi dan Misi dibentuknya Forum PUSPA sebagai mitra strategis pemerintah dalam memecahkan persoalan perempuan dan anak yang sifatnya koordinasi. </p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 col-xl-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Menu</h4>
+                    <ul>
+                        <li><a href="{{ url('/') }}">Beranda</a></li>
+                        <li><a href="{{ url('/visi-misi') }}">Visi dan Misi</a></li>
+                        <li><a href="{{ url('/structure-organization') }}">Struktur Organisasi</a></li>
+                        <li><a href="{{ url('/gallery-photo') }}">Galeri</a></li>
+                        <li><a href="{{ url('/news') }}">Berita</a></li>
+                        <li><a href="{{ url('/work-program') }}">Program Kerja</a></li>
+                        <li><a href="{{ url('/services') }}">Layanan</a></li>
+                    </ul>
+                    
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 col-md-4">
+                <div class="single-footer-widget footer_2">
+                    <h4>Hubungi Kami</h4>
+                    <div class="contact_info">
+                        <p><span> Alamat :</span> Bandung, Jawa Barat </p>
+                        <p><span> Telepon :</span> +62 36 265 (8060)</p>
+                        <p><span> Email : </span>humas@forkom-puspajabar.or.id </p>
+                    </div>
+                    <div class="social_icon">
+                        <a href="#"> <i class="ti-facebook"></i> </a>
+                        <a href="#"> <i class="ti-twitter-alt"></i> </a>
+                        <a href="#"> <i class="ti-instagram"></i> </a>
+                        <a href="#"> <i class="ti-youtube"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="copyright_part_text text-center">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Forum Komunikasi Puspa Jawa Barat
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
